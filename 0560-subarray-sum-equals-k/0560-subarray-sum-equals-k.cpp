3e4fd9c8f8ef7ce9e656f7798b8bf8061a28425e
Himanshu Srivastava <himanshusrivastava5390@gmail.com>
2024-09-15 13:26:46 +0530

    Time: 65 ms (72.06%), Space: 45.3 MB (69.82%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int subarraySum(vector<int>& arr, int k) {
         
-       
+       //  METHOD 1
+        
 //         // map
 //         int cnt = 0;
 //         long long sum = 0;
-//         map<long long, int>mpp;
+        // map<long long, int>mpp;
         
 //         for(int i = 0; i< nums.size();i++){
             
@@ -26,21 +27,50 @@ public:
         
 //         return cnt;
         
-        int n= arr.size();
-        int cnt= 0;
+        
+        // METHOD 2
+        
+        
+//         int n= arr.size();
+//         int cnt= 0;
 
-        for(int i=0; i<n; i++)
-        {
-            int sum= 0;
-            for(int j=i; j<n; j++)
-            {
-                sum+= arr[j];
-               if(sum== k)
-               {
-                 cnt++;
-               }
+//         for(int i=0; i<n; i++)
+//         {
+//             int sum= 0;
+//             for(int j=i; j<n; j++)
+//             {
+//                 sum+= arr[j];
+//                if(sum== k)
+//                {
+//                  cnt++;
+//                }
+//             }
+//         }
+//        return cnt;
+        
+        
+        // METHOD 3
+        
+        int prefixSum = 0;
+        int cnt = 0;
+        
+        unordered_map<int,int>m;
+        m[prefixSum] = 1;
+        for(auto it : arr){
+            
+            prefixSum+=it;
+            if(m.find(prefixSum-k)!= m.end()){
+                cnt+=m[prefixSum-k];
             }
+            m[prefixSum]++;
         }
-       return cnt;
+        
+        return cnt;
+        
+    
+        
+        
+        
     }
 };
+
