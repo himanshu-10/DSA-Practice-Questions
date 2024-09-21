cb8cc9c468cc3930a8b3ffc5c96601a245767fea
Himanshu Srivastava <himanshusrivastava5390@gmail.com>
2024-09-21 23:50:28 +0530

    Time: 16 ms (67.14%), Space: 26.9 MB (79.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,18 +2,30 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        map<int,int>mpp;
-        int ans = 0;
+        // MOORE VOTING ALGO
+        
+        int elem;
+        int cnt = 0;
+        
         for(int i = 0; i< nums.size();i++){
-            mpp[nums[i]]++;
+            
+            if(cnt == 0){
+                cnt = 1;
+                elem = nums[i];
+            }else 
+            if(nums[i] == elem) cnt++;
+            else cnt--;
         }
         
-        
-        for(auto it : mpp){
-            if(it.second > nums.size()/2 ){
-                ans =  it.first;
-            }
+        int cnt1 = 0;
+        int ans;
+        for(int i = 0; i< nums.size();i++){
+            if(nums[i] == elem) cnt1++;
         }
-        return ans;
+        
+        if(cnt1 > (nums.size()/2)) ans =  elem;
+        
+        return ans
+            ;
     }
 };
